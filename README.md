f7d9360deaee87f9f2266e9a0a9731e7ef1fb910
Eric Chen <ericchen201896@gmail.com>
2020-12-15 20:08:15 -0800

    Added in comments

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # Code_Quiz
+
+# Random_Password_Generator
+
+
+![Code Quiz](.mainScreen_Screenshot.png)
+
+[Link to site](https://ericchen96.github.io/Random_Password_Generator/) <br>
+[Link to Github repository](https://github.com/EricChen96/Random_Password_Generator)
+
+## Installation
+
+Go to Github and download
+
+## Description
+
+The following password generator would prompt the user for the length of their password, ask them for whether they want lowercase, uppercase, numeric or special characters, and then generate a password. 
+
+## Additions:
+
+1. If the user confirms they want a type of character, the generator would ensure that one character from that type is in the password. The function checkIfCharUsed(checkCharacterUsed, checkPassword, checkCharacterSheet) returns true if the password uses that character. It accepts a boolean first to see if user wanted that type of character to be used. Then accepts the password to check, and the characters to check for. 
+
+``` 
+function checkIfCharUsed(checkCharacterUsed, checkPassword, checkCharacterSheet) {
+  if (checkCharacterUsed) {
+    for (var i = 0; i < checkPassword.length; i++) {
+      for (var j = 0; j < checkCharacterSheet.length; j++) {
+        if (checkPassword[i] === checkCharacterSheet[j]) {
+          return true
+        }
+      }
+    }
+  }
+  else {
+    return true;
+  }
+  return false;
+}
+```
+
+This code block uses the checkIfCharUsed function to check the password and would loop to create a new password until the password contains a character type that the user selected
+
+```
+do {
+    tempPassword = "";
+    for (var i = 0; i < passwordLength; i++) {
+      tempPassword += characterSheet[Math.floor(Math.random() * characterSheet.length)];
+    }
+  } while (!checkIfCharUsed(allowLowercase, tempPassword, lowercaseSheet) || !checkIfCharUsed(allowUppercase, tempPassword, uppercaseSheet) || !checkIfCharUsed(allowNumeric, tempPassword, numericSheet) || !checkIfCharUsed(allowSpecial, tempPassword, specialSheet))
+```
+
+2. The other addition was an auto-copy to clipboard once the password was generated
+```
+  var copyText = document.querySelector("#password");
+  copyText.focus();
+  copyText.select();
+  document.execCommand('copy');
+```
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//contains quiz section of app
 var quizContainerEl = document.querySelector("#quizContainerEl");
+//header containing highscore link
 var headerEl = document.querySelector("#headerEl");
 var highscoresEl = document.querySelector("#highscoreEl");
 
 //Question Game elements
 var questionEl = document.querySelector("#questionEl");
 var optionsEl = document.querySelector("#options-list");
-
+var message = document.createElement("h3");
+message.setAttribute("class","isCorrectMessage");
 
 var questionCount;
 var timeLeft;
@@ -23,58 +26,56 @@ gameInstructions.textContent = "Welcome to Code Quiz. This game tests your Javas
 startButton.setAttribute("class", "startButton")
 gameTitle.setAttribute("class", "gameTitle");
 
-
+//End screen elements
 var scoreDisplay = document.createElement("h2");
 var initialForm = document.createElement("form");
 var initialInput = document.createElement("input");
 var playAgainButton = document.createElement("button");
-
-
 playAgainButton.setAttribute("class", "playAgainButton")
 
 //Highscores page elements
 var highscoresList = document.createElement("ol");
 var highscoresTitle = document.createElement("h2");
 var resetHighscoresButton = document.createElement("button");
-highscoresList.setAttribute("class","highscoresList");
+highscoresList.setAttribute("class", "highscoresList");
 playAgainButton.textContent = "Play Again";
 highscoresTitle.textContent = "HIGHSCORES";
 resetHighscoresButton.textContent = "Reset Highscores";
-resetHighscoresButton.setAttribute("class","resetButton");
-highscoresTitle.setAttribute("class","highscoresTitle");
+resetHighscoresButton.setAttribute("class", "resetButton");
+highscoresTitle.setAttribute("class", "highscoresTitle");
 
+//question array of objects. q is the question, o are options, 
 var questions = [
     { q: "The data type to store 'true' or 'false' is: ", o: ["String", "Integer", "Boolean", "Objects"], a: "Boolean" },
     { q: "Array items should be enclosed in: ", o: ["[ ]", "{ }", "\" \"", "Nothing"], a: "[ ]" },
     { q: "To obtain the third item in an array, the correct notation is: ", o: ["array[0]", "array[1]", "array[2]", "array[3]"], a: "array[2]" },
-    { q: "To loop through to the final index, what should the condition be in the for loop: ", o: ["i<=array.length", "i<1000", "i<array.length-1", "i<array.length"], a: "i<array.length" }
+    { q: "To loop through to the final index, what should the condition be in the for loop: ", o: ["i<=array.length", "i<1000", "i<array.length-1", "i<array.length"], a: "i<array.length"},
+    { q: "To close a line of code, you should use: ", o: [";", ".end",":","."], a: ";"}
 ];
 
+//Creates title page showing title, instructions and start button
 function createMainScreen() {
     quizContainerEl.innerHTML = "";
     quizContainerEl.append(gameTitle);
     quizContainerEl.append(gameInstructions);
     quizContainerEl.append(startButton);
-    startButton.addEventListener("click", startGame);
 }
 
-function init() {
-    var storedHighscores = JSON.parse(localStorage.getItem("highscores"));
-    if (storedHighscores !== null) {
-        highscores = storedHighscores;
-    }
-}
+//Start button action listener
+startButton.addEventListener("click", startGame);
 
-
+//Changes the timer at top right corner
 function changeTimeleft() {
     headerEl.children[1].textContent = timeLeft;
 }
 
+//Starts the game reseting timeLeft, questionCount, and countdown
 function startGame() {
     quizContainerEl.innerHTML = "";
     questionCount = 0;
     timeLeft = 10;
     loadQuestion(questionCount);
+
     timeInterval = setInterval(function () {
         if (timeLeft < 0 || questionCount === questions.length) {
             clearInterval(timeInterval);
@@ -89,15 +90,15 @@ function startGame() {
     }, 1000)
 }
 
-
+//Loads the question according to what count we are on
 function loadQuestion(questionCount) {
     quizContainerEl.innerHTML = "";
-
-
     quizContainerEl.append(questionEl);
     quizContainerEl.append(optionsEl);
+    quizContainerEl.append(message);
     questionEl.textContent = questions[questionCount].q;
     optionsEl.innerHTML = "";
+    //Option buttons
     for (var i = 0; i < questions[questionCount].o.length; i++) {
         var option = document.createElement("button");
         option.setAttribute("class", "gameQuestionButtons")
@@ -106,15 +107,25 @@ function loadQuestion(questionCount) {
     }
 }
 
+//If clicking within the optionEl, checks if correct, updates time, and shows message for incorrect or correct
 optionsEl.addEventListener("click", function (event) {
     var element = event.target;
     if (event.target.textContent === questions[questionCount].a) {
         timeLeft += 10;
         changeTimeleft();
+        showAnswerMessage(true);
     }
     else {
-        timeLeft -= 10;
-        changeTimeleft();
+        if (timeLeft <= 10) {
+            clearInterval(timeInterval);
+            timeLeft = 0;
+            changeTimeleft();
+        }
+        else {
+            timeLeft -= 10;
+            changeTimeleft();
+        }
+        showAnswerMessage(false);
     }
     questionCount++;
     if (questionCount === questions.length || timeLeft <= 0) {
@@ -125,6 +136,27 @@ optionsEl.addEventListener("click", function (event) {
     }
 })
 
+//Shows a 1 second message according to if user answers correctly of incorrectly
+function showAnswerMessage(isCorrect) {
+    var isCorrectInterval = 1;
+    var solutionInterval = setInterval(function () {
+        if(isCorrectInterval > 0 ){
+            if (isCorrect) {
+                message.textContent = "Correct";
+            }
+            else if(isCorrect === false){
+                message.textContent = "Incorrect";
+            }
+            isCorrectInterval--;
+        }
+        else {
+            clearInterval(solutionInterval)
+            message.textContent = "";
+        }
+    }, 1000)
+}
+
+//Shows ending screen if user reaches end of game or time runs out
 function endGame() {
     quizContainerEl.innerHTML = "";
     score = timeLeft;
@@ -137,10 +169,12 @@ function endGame() {
     quizContainerEl.append(playAgainButton);
 }
 
+//Play again button
 playAgainButton.addEventListener("click", function () {
     startGame();
 })
 
+//If user submits their id, records the score in highscore list 
 initialForm.addEventListener("submit", function (event) {
     event.preventDefault();
     var initialText = initialInput.value.trim();
@@ -148,6 +182,7 @@ initialForm.addEventListener("submit", function (event) {
         return;
     }
     var tempScore = { initial: initialText, playerScore: score };
+    //if there are 10 highscores stored, it will remove the first one and add new one to end of list
     if (highscores.length < 10 || highscores === null) {
         highscores.push(tempScore);
         storeHighscores();
@@ -160,14 +195,17 @@ initialForm.addEventListener("submit", function (event) {
     initialInput.value = "";
 })
 
+//stores highscores array in local storage
 function storeHighscores() {
     localStorage.setItem("highscores", JSON.stringify(highscores));
 }
 
+//if user clicks top left corner on "highscore", it goes to highscores page
 highscoresEl.addEventListener("click", function () {
     loadHighscores();
 })
 
+//clears interval if user clicked while in quiz and loads list of highscores
 function loadHighscores() {
     clearInterval(timeInterval);
     quizContainerEl.innerHTML = "";
@@ -184,12 +222,21 @@ function loadHighscores() {
     quizContainerEl.append(resetHighscoresButton);
 }
 
-resetHighscoresButton.addEventListener("click", function() {
+//initialization function for getting highscore information from local storage and storing it in our array
+function init() {
+    var storedHighscores = JSON.parse(localStorage.getItem("highscores"));
+    if (storedHighscores !== null) {
+        highscores = storedHighscores;
+    }
+}
+
+//Reset highscore array
+resetHighscoresButton.addEventListener("click", function () {
     highscores = [];
     storeHighscores();
     loadHighscores();
 })
 
-
+//Calling functions to load game
 init();
 createMainScreen();
